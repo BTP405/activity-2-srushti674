1fdc0a9b3b77840e576f125a0ab6b721a27abc02
srushti674 <123048710+srushti674@users.noreply.github.com>
2024-02-14 15:15:56 -0500

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,1 +1,15 @@
 def graphSnowfall(t):
+    try:
+        with open(t, 'r') as file:
+            data = file.readlines()
+            cities = [line.split()[0] for line in data]
+            snowfalls = [float(line.split()[1]) for line in data]
+
+            plt.bar(cities, snowfalls, color='blue')
+            plt.xlabel('Cities')
+            plt.ylabel('Snowfall (inches)')
+            plt.title('Snowfall Data')
+            plt.show()
+
+    except FileNotFoundError:
+        print(f"Error: File '{t}' not found.")
