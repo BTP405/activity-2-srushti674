d7979e8c51f5dfd1f8dd020baac51bf8fb02e5ff
srushti674 <123048710+srushti674@users.noreply.github.com>
2024-02-14 15:18:00 -0500

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,1 +1,25 @@
+def printStatsDecorator(func):
+    def wrapper(numbers):
+        print("Numbers read: ", numbers)
+        print("The count of the number read: ", len(numbers))
+        print("Average: ", sum(numbers) / len(numbers))
+        print("Maximum: ", max(numbers))
+    return wrapper
+
 def printStats(t):
+    text_file = open(t, 'r')
+
+    for line in text_file: 
+        numbers = list(map(int, line.split())) 
+
+        # this function is decorated to print the statistics
+        @printStatsDecorator
+        def passLines(numbers): 
+            pass 
+
+        # passing the array to the decorator
+        passLines(numbers)
+
+    text_file.close()
+
+printStats('Q4.txt')
